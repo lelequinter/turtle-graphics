82c6f4d5a53a97db2b355c1b9e8e3a5e227e80c1
Leyder Quintero <desarrollador9@wedevel.co>
2025-01-13 16:59:30 -0500

    Dashed line

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from turtle import Turtle, Screen
 tim = Turtle()
 
 tim.shape('turtle')
-tim.color('midnightBlue')
 
-for _ in range(4):
-    tim.forward(100)
-    tim.right(90)
+#for _ in range(4):
+#    tim.forward(100)
+#    tim.right(90)
+
+for num in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
 
 screen = Screen()
 screen.exitonclick()
